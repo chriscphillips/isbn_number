fd9fd964260b9250d27dfad4dce37af572f41e88
chriscphillips <cptp50wv@shentel.net>
2017-12-13 13:55:04 -0500

    added isbn 13

--- isbn2.rb
+++ isbn2.rb
@@ -2,7 +2,7 @@
 puts "Enter your ISBN number:"
 #new line asking user for input
 isbn_number = gets.chomp
-# numbers are displayed in an array different from the users input
+# # The map method takes an enumerable object and a block, and runs the block for each element, outputting each returned value from the block
 isbn_array = isbn_number.split("").map(&:to_i)
 #printing the array
 print isbn_array
@@ -15,31 +15,46 @@ def initial_isbn_valid(isbn_array)
     	valid_10_digit_isbn(isbn_array)
     #count the isbn number for thirteen numbers, and prints 	
     elsif isbn_count == 13
-    	puts "13 digits"
+    	valid_13_digit_isbn(isbn_array)
     # prints that the number is not valid
     else
-        puts "not an isbn number"
+        puts "not an isbn number. it must be 10 or 13 digits"
     end
 end
  
     # function definition to verifity isbn number
-    def valid_10_digit_isbn(isbn_array)
-    	isbn_number_addition_total = 0
-        count_isbn = 10
-        while count_isbn > 1
-        isbn_array.each do |digits|
-        isbn_number_addition_total = isbn_number_addition_total + (digits * count_isbn)
-    	count_isbn = count_isbn - 1
+def valid_10_digit_isbn(isbn_array)
+    isbn_number_addition_total = 0
+    count_isbn = 10
+    while count_isbn > 1
+	    isbn_array.each do |digits|
+	    isbn_number_addition_total = isbn_number_addition_total + (digits * count_isbn)
+	    count_isbn = count_isbn - 1
     	end
-    end	    	
+	end	    	
     	
-    	if isbn_number_addition_total % 11 == 0
-    		puts "this is a valid 10 digit isbn number"
-    	else
-    		puts "this is not a valid isbn number"
-    	end
+ 	if isbn_number_addition_total % 11 == 0
+   		puts "this is a valid 10 digit isbn number"
+   	else
+   		puts "this is not a valid isbn number"
+   	end
 end
+
 def valid_13_digit_isbn(isbn_array)
-	end
-	
+	count = 0
+    array_addition_total = 0
+    multiply_array = [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1] 
+    isbn_array.each do |digits|
+    	array_addition_total = array_addition_total + (digits * multiply_array[count])
+        count = count + 1
+    end
+
+	if array_addition_total % 10 == 0
+    		puts "this is a valid 13 digit isbn number"
+    else
+    		puts "this is not a valid 13 isbn number"
+    end 
+end
+
+    
 initial_isbn_valid(isbn_array)
